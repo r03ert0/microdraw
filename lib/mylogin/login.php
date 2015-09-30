6015b9a5c646e8ca35b173cd6faaa7e8c240b9a0
Hannah Spitzer <h.spitzer@fz-juelich.de>
2015-09-30 13:17:49 +0200

    Encapsulated access to mysql DB in file php/db_access.php

--- lib/mylogin/login.php
+++ lib/mylogin/login.php
@@ -12,9 +12,7 @@ $rootdir = "/microdraw";
 
 //include $_SERVER['DOCUMENT_ROOT'].$rootdir."/php/base.php";
 session_start();
-$dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
-$dbuser = "microdraw"; /*ENTER YOUR DB USER HERE*/; // the username that you created, or were given, to access your database
-$dbpass = "microdraw"; /*ENTER YOUR DB PASSWORD HERE*/; // the password that you created, or were given, to access your database
+include('../../php/db_access.php');
 $dblogin = "MyUsers";
 $connection=mysqli_connect($dbhost,$dbuser,$dbpass,$dblogin) or die("ERROR: Can't connect to MySQL DB: " . mysql_error());
 
