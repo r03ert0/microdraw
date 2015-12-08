4a03762543a66ce35fb5645e203b062d774c3237
roberto <roberto@ramses.ghfc.pasteur.fr>
2015-12-08 15:08:21 +0100

    update DB generation code

--- php/microdraw_db.sql
+++ php/microdraw_db.sql
@@ -2,9 +2,12 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `KeyValue` (
+  `UniqueID` int(11) NOT NULL AUTO_INCREMENT,
   `myTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `myOrigin` text COLLATE utf8_bin NOT NULL,
   `myKey` text COLLATE utf8_bin NOT NULL,
   `myValue` text COLLATE utf8_bin NOT NULL,
-  UNIQUE KEY `myTimestamp` (`myTimestamp`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  PRIMARY KEY (`UniqueID`),
+  UNIQUE KEY `UniqueID` (`UniqueID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=100 ;
+
