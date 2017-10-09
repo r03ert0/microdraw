a6678f8cef8618653cf17b25e57e6770e9f4861f
r03ert0 <rto@braincatalogue.org>
2017-10-09 15:18:07 +0200

    add data controller

--- controller/data/data.controller.js
+++ controller/data/data.controller.js
@@ -1,11 +1,19 @@
 "use strict";
 
-
 var data = function data(req, res) {
     var source = req.query.source;
+    var login = (req.isAuthenticated()) ?
+                ("<a href='/user/" + req.user.username + "'>" + req.user.username + "</a> (<a href='/logout'>Log Out</a>)")
+                : ("<a href='/auth/github'>Log in with GitHub</a>");
+    var loggedUser = req.isAuthenticated()?req.user.username:"anonymous";
+
+    // store return path in case of login
+    req.session.returnTo = req.originalUrl;
+
     res.render('data', {
         title: 'MicroDraw::Data',
-        params: JSON.stringify(req.query)
+        params: JSON.stringify(req.query),
+        login: login
     });
 };
 
