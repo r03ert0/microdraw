5774848efe9a438a190c7f1c424b9b55bfad9303
Xiao Gui <xgui3783@gmail.com>
2017-10-18 13:31:49 +0200

    rebase

--- controller/data/data.controller.js
+++ controller/data/data.controller.js
@@ -1,11 +1,7 @@
 "use strict";
 
 function data(req, res) {
-<<<<<<< ec576397d7f1d4204e9eebd0bf27451d147c648f
     var { source: source } = req.query; // eslint-disable-line no-unused-vars, no-useless-rename
-=======
-    var { source } = req.query; // eslint-disable-line no-unused-vars
->>>>>>> init commit for overcoming cors header
     var login = (req.isAuthenticated()) ?
                 ("<a href='/user/" + req.user.username + "'>" + req.user.username + "</a> (<a href='/logout'>Log Out</a>)")
                 : ("<a href='/auth/github'>Log in with GitHub</a>");
