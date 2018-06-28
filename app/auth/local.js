60b189de1a46e5f8d910caf1079ce3f775eb773d
Xiao Gui <xgui3783@gmail.com>
2018-06-28 16:28:08 +0200

    local signin

--- app/auth/local.js
+++ app/auth/local.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
+const md5 = require('md5')
 
 const saltRounds = 10
 
@@ -8,23 +9,33 @@ module.exports = (app)=>{
 
   passport.use(new LocalStrategy(
     (username,password,done)=>{
+      console.log('querying db')
       app.db.queryUser({
-        strategy : 'local',
         username 
       })
         .then(user=>{
-          bcrypt.compare(password,user.passhash)
-            .then(res=>done(null,res ? user : false))
-            .catch(e=>done(e))
+          done(null, user ? 
+            user.password === md5(password) ?
+              user :
+              false :
+            false)
+
+          /* bcrypt -> more secure */
+          // bcrypt.compare(password,user.passhash)
+          //   .then(res=>done(null,res ? user : false))
+          //   .catch(e=>done(e))
         })
-        .catch(e=>done(e))
+        .catch(e=>{
+          console.log(JSON.stringify(e))
+          done(e)
+        })
     }
   ))
 
   app.post(
     '/localLogin',
     passport.authenticate('local',{
-      failureRedirect : '/'
+      failureRedirect : '/html/localsignin.html'
     }),
     (req,res)=>{
       res.redirect(301,req.session.returnTo || '/')
@@ -33,18 +44,21 @@ module.exports = (app)=>{
 
   app.post('/localSignup', (req,res)=>{
     console.log(req.body)
-    bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{
-      if(err) res.status(500).send(err)
-      else {
+    res.status(404)
 
-        /* save req.body.username & hash */
-        app.db.addUser({
-          strategy : 'local',
-          username : req.body.username,
-          passhash : hash
-        })
-        res.status(200)
-      }
-    })
+    /* bcrypt more secrure */
+    // bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{
+    //   if(err) res.status(500).send(err)
+    //   else {
+
+    //     /* save req.body.username & hash */
+    //     app.db.addUser({
+    //       strategy : 'local',
+    //       username : req.body.username,
+    //       passhash : hash
+    //     })
+    //     res.status(200)
+    //   }
+    // })
   })
 }
