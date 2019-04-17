ac7f7bd7e854e3b85291235ca605f1771f874d4b
r03ert0 <rto@braincatalogue.org>
2019-04-17 12:11:33 +0200

    new feature: upload annotations programmatically

--- app/controller/api/index.js
+++ app/controller/api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
 
 // API routes
 router.get('', function (req, res) {
@@ -19,24 +22,62 @@ router.get('', function (req, res) {
         .catch(e=>res.state(500).send({err:JSON.stringify(e)}))
 });
 
-router.post('', function (req, res) {
+const saveFromGUI = function (req, res) {
+    const { source, slice, Hash, annotation } = req.body;
 
-    console.warn("call to POST api");
+    const user = (req.user && req.user.username) || 'anonymous';
 
-    if(req.body.action === 'save') {
-        const { source, slice, Hash, annotation } = req.body;
+    req.app.db.updateAnnotation({
+        fileID : `${source}&slice=${slice}`,
+        user,
+        Hash,
+        annotation
+    })
+        .then(() => res.status(200).send())
+        .catch((e) => res.status(500).send({err:JSON.stringify(e)}));
+};
 
-        const user = (req.user && req.user.username) || 'anonymous';
+const saveFromAPI = function (req, res) {
+    const user = req.user && req.user.username;
+
+    if (typeof user === 'undefined') {
+        res.status(401).send({msg:'API upload requires a valid token authentication'});
+    } else {
+        const { source, slice, Hash } = req.query;
+        const fileID = `${source}&slice=${slice}`;
+        const json = JSON.parse(fs.readFileSync(req.files[0].path).toString());
+        const annotations = {Regions: []};
+        for(let ann of json) {
+            annotations.Regions.push(ann.annotation);
+        }
 
         req.app.db.updateAnnotation({
-            fileID : `${source}&slice=${slice}`,
+            fileID : fileID,
             user,
             Hash,
-            annotation
+            annotation: JSON.stringify(annotations)
         })
             .then(() => res.status(200).send())
             .catch((e) => res.status(500).send({err:JSON.stringify(e)}));
-    }else{
+    }
+};
+
+router.post('', function (req, res) {
+    console.warn("call to POST from GUI");
+
+    if(req.body.action === 'save') {
+        saveFromGUI(req, res);
+    } else {
+        res.status(500).send({err:'actions other than save are no longer supported.'});
+    }
+});
+
+router.post('/upload', multer({dest: path.join(__dirname, 'tmp')}).array('data'), function (req, res) {
+    console.warn("call to POST from API");
+
+    if(req.query.action === 'save') {
+        saveFromAPI(req, res);
+    } else {
         res.status(500).send({err:'actions other than save are no longer supported.'});
     }
 });
