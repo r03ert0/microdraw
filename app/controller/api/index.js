4db5b2377754c38ebb856b17491d9f5c26649963
r03ert0 <rto@braincatalogue.org>
2019-08-20 15:28:23 +0200

    linting

--- app/controller/api/index.js
+++ app/controller/api/index.js
@@ -37,27 +37,57 @@ const saveFromGUI = function (req, res) {
         .catch((e) => res.status(500).send({err:JSON.stringify(e)}));
 };
 
+/**
+ * Tests if an object is a valid annotation.
+ * @param {object} obj An annotation object to validate.
+ * @returns {boolean} True if the object is valid
+ */
+const validateAnnotation = function (obj) {
+    if(typeof obj === 'undefined') {
+        return false;
+    } else if(obj.constructor !== Array) {
+        return false;
+    } else if(typeof obj[0].path === 'undefined') {
+        return false;
+    }
+
+    return true;
+};
+
+/**
+ * Loads a json file containing an annotation object.
+ * @param {string} annotationPath Path to json file containing an annotation
+ * @returns {object} A valid annotation object or nothing.
+ */
+const loadAnnotationFile = function (annotationPath) {
+    const json = JSON.parse(fs.readFileSync(annotationPath).toString());
+    if(validateAnnotation(json) === true) {
+        return json;
+    }
+};
+
 const saveFromAPI = function (req, res) {
     const user = req.user && req.user.username;
+    const { source, slice, Hash } = req.query;
+    const json = loadAnnotationFile(req.files[0].path);
 
     if (typeof user === 'undefined') {
-        res.status(401).send({msg:'API upload requires a valid token authentication'});
+        res.status(401).send({msg: "API upload requires a valid token authentication"});
+    } else if(typeof json === "undefined") {
+            res.status(401).send({msg: "Invalid annotation file"});
     } else {
-        const { source, slice, Hash } = req.query;
-        const fileID = `${source}&slice=${slice}`;
-        const json = JSON.parse(fs.readFileSync(req.files[0].path).toString());
         const annotations = {Regions: []};
-        for(let ann of json) {
+        for(const ann of json) {
             annotations.Regions.push(ann.annotation);
         }
 
         req.app.db.updateAnnotation({
-            fileID : fileID,
+            fileID: `${source}&slice=${slice}`,
             user,
             Hash,
             annotation: JSON.stringify(annotations)
         })
-            .then(() => res.status(200).send())
+            .then(() => res.status(200).send({msg: "Annotation successfully saved"}))
             .catch((e) => res.status(500).send({err:JSON.stringify(e)}));
     }
 };
