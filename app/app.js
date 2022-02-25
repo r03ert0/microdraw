acbeb52e6e44082737dae16c21cb3461e1cc6651
Nicolas <ntraut9@gmail.com>
2022-02-25 13:34:49 +0100

    set cors headers

--- app/app.js
+++ app/app.js
@@ -97,6 +97,21 @@ const start = async function () {
   });
 
 
+  // CORS
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*'); // 'http://localhost:8888');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+  });
+
   // uncomment after placing your favicon in /public
   //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   if (process.env.NODE_ENV !== 'production') {
@@ -105,7 +120,6 @@ const start = async function () {
   app.use(bodyParser.json({ limit: '50mb', extended: true }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.use(express.static(path.join(__dirname, 'public')));
-
 
   /* setup DB */
   // var checkAnyoneUser = function () {
