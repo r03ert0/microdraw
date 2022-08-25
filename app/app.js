06f66f1ca8e55fc0c001a8bf6b7802ed1214bf1c
roberto <roberto.toro@gmail.com>
2022-08-25 23:32:48 +0200

    tone down on error display

--- app/app.js
+++ app/app.js
@@ -163,9 +163,9 @@ const start = async function () {
   // catch 404 and forward to error handler
   app.use(function (req, res, next) {
     console.log('ERROR: File not found', req.url);
-    var err = new Error('Not Found', req);
-    err.status = 404;
-    next(err);
+    // var err = new Error('Not Found'); //, req);
+    // err.status = 404;
+    // next(err);
   });
 
   // error handler
