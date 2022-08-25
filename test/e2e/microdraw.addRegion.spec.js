f02b6d42415f2071e5c0b08c80f15596a1d25acf
roberto <roberto.toro@gmail.com>
2022-08-25 19:21:34 +0200

    dom instead of pixels

--- test/e2e/microdraw.addRegion.spec.js
+++ test/e2e/microdraw.addRegion.spec.js
@@ -29,11 +29,16 @@ describe('Editing tools: Add regions', () => {
   it('opens a data page', async () => {
     page = await browser.newPage();
     await page.setViewport({width: U.width, height: U.height});
-    const diff = await U.comparePageScreenshots(
-      page,
+    await page.goto(
       'http://localhost:3000/data?source=/test_data/cat.json&slice=0',
-      'addRegion.01.cat.png'
+      { waitUntil: 'networkidle0' }
     );
+    await U.waitUntilHTMLRendered(page);
+    // const diff = await U.comparePageScreenshots(
+    //   page,
+    //   'http://localhost:3000/data?source=/test_data/cat.json&slice=0',
+    //   'addRegion.01.cat.png'
+    // );
     // assert(diff<1000, `${diff} pixels were different`);
   }).timeout(0);
 
@@ -55,7 +60,7 @@ describe('Editing tools: Add regions', () => {
       regionsLength: Microdraw.ImageInfo[0].Regions.length,
       pathSegments: Microdraw.ImageInfo[0].Regions[0].path.segments.length
     }));
-    console.log(res);
+    // console.log(res);
     assert(res.regionsExists === true, 'No Regions object');
     assert(res.regionsLength === 1, `Regions.length is ${res.regionsLength} instead of 1`);
     assert(res.pathSegments === 4, `Path has ${res.pathSegments} segments instead of 4`);
@@ -65,6 +70,7 @@ describe('Editing tools: Add regions', () => {
     // assert(diff<U.pct5, `${diff} pixels were different - more than 5%`);
   }).timeout(0);
 
+  // eslint-disable-next-line max-statements
   it('draws another square', async () => {
     // draw a square B
     await page.mouse.click(450, 450);
@@ -81,8 +87,7 @@ describe('Editing tools: Add regions', () => {
       path1Segments: Microdraw.ImageInfo[0].Regions[0].path.segments.length,
       path2Segments: Microdraw.ImageInfo[0].Regions[1].path.segments.length
     }));
-    console.log(res);
-
+    // console.log(res);
     assert(res.regionsExists === true, 'No Regions object');
     assert(res.regionsLength === 2, `Regions.length is ${res.regionsLength} instead of 2`);
     assert(res.path1Segments === 4, `1st path has ${res.path1Segments} segments instead of 4`);
@@ -109,8 +114,7 @@ describe('Editing tools: Add regions', () => {
       regionsLength: Microdraw.ImageInfo[0].Regions.length,
       pathSegments: Microdraw.ImageInfo[0].Regions[0].path.segments.length
     }));
-    console.log(res);
-
+    // console.log(res);
     assert(res.regionsExists === true, 'No Regions object');
     assert(res.regionsLength === 1, `Regions.length is ${res.regionsLength} instead of 1`);
     assert(res.pathSegments === 8, `Path has ${res.pathSegments} segments instead of 4`);
--- test/e2e/microdraw.copy-paste.spec.js
+++ test/e2e/microdraw.copy-paste.spec.js
@@ -29,12 +29,17 @@ describe('Editing tools: Copy and paste', () => {
   it('opens a data page', async () => {
     page = await browser.newPage();
     await page.setViewport({width: U.width, height: U.height});
-    const diff = await U.comparePageScreenshots(
-      page,
+    await page.goto(
       'http://localhost:3000/data?source=/test_data/cat.json&slice=0',
-      'copyPaste.01.cat.png'
+      { waitUntil: 'networkidle0' }
     );
-    assert(diff<1000, `${diff} pixels were different`);
+    await U.waitUntilHTMLRendered(page);
+    // const diff = await U.comparePageScreenshots(
+    //   page,
+    //   'http://localhost:3000/data?source=/test_data/cat.json&slice=0',
+    //   'copyPaste.01.cat.png'
+    // );
+    // assert(diff<1000, `${diff} pixels were different`);
   }).timeout(0);
 
   it('draws a square', async () => {
@@ -47,10 +52,20 @@ describe('Editing tools: Copy and paste', () => {
     await page.mouse.click(400, 500);
     await page.mouse.click(400, 400);
 
-    const filename = "copyPaste.02.cat-square.png";
-    await page.screenshot({path: U.newPath + filename});
-    const diff = await U.compareImages(U.newPath + filename, U.refPath + filename);
-    assert(diff<U.pct5, `${diff} pixels were different - more than 5%`);
+    const res = await page.evaluate(() => ({
+      regionsExists: typeof (Microdraw.ImageInfo[0].Regions) !== 'undefined',
+      regionsLength: Microdraw.ImageInfo[0].Regions.length,
+      pathSegments: Microdraw.ImageInfo[0].Regions[0].path.segments.length
+    }));
+    // console.log(res);
+    assert(res.regionsExists === true, 'No Regions object');
+    assert(res.regionsLength === 1, `Regions.length is ${res.regionsLength} instead of 1`);
+    assert(res.pathSegments === 4, `Path has ${res.pathSegments} segments instead of 4`);
+
+    // const filename = "copyPaste.02.cat-square.png";
+    // await page.screenshot({path: U.newPath + filename});
+    // const diff = await U.compareImages(U.newPath + filename, U.refPath + filename);
+    // assert(diff<U.pct5, `${diff} pixels were different - more than 5%`);
   }).timeout(0);
 
   // eslint-disable-next-line max-statements
@@ -69,10 +84,22 @@ describe('Editing tools: Copy and paste', () => {
 
     await shadowclick(UI.PASTE);
 
-    const filename = "copyPaste.03.cat-paste.png";
-    await page.screenshot({path: U.newPath + filename});
-    const diff = await U.compareImages(U.newPath + filename, U.refPath + filename);
-    assert(diff<U.pct5, `${diff} pixels were different - more than 5%`);
+    const res = await page.evaluate(() => ({
+      regionsExists: typeof (Microdraw.ImageInfo[0].Regions) !== 'undefined',
+      regionsLength: Microdraw.ImageInfo[0].Regions.length,
+      path1Segments: Microdraw.ImageInfo[0].Regions[0].path.segments.length,
+      path2Segments: Microdraw.ImageInfo[0].Regions[1].path.segments.length
+    }));
+    // console.log(res);
+    assert(res.regionsExists === true, 'No Regions object');
+    assert(res.regionsLength === 2, `Regions.length is ${res.regionsLength} instead of 2`);
+    assert(res.path1Segments === 4, `1st path has ${res.path1Segments} segments instead of 4`);
+    assert(res.path2Segments === 4, `2nd path ${res.path2Segments} segments instead of 4`);
+
+    // const filename = "copyPaste.03.cat-paste.png";
+    // await page.screenshot({path: U.newPath + filename});
+    // const diff = await U.compareImages(U.newPath + filename, U.refPath + filename);
+    // assert(diff<U.pct5, `${diff} pixels were different - more than 5%`);
   }).timeout(0);
 
   after(async () => {
